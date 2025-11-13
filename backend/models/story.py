b52e4cf0e1aeba34a0bdc11b1cb126cc4260ef03
Liyacommits <liyabonamendela3@gmail.com>
2025-11-13 17:16:13 +0200

    have to learn databases

--- backend/models/story.py
+++ backend/models/story.py
@@ -2,3 +2,15 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, J
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
+from db.database import Base
+
+class Story(Base):
+    __tablename__ = "stories"
+
+    id = Column(Integer, primary_key=True, index=True) # Unique identifier for each story index means it will be indexed for faster searching
+    title = Column(String, index=True) # Title of the story
+    session_id = Column(String, index=True) # Session identifier to group stories by user session
+    created_at = Column(DateTime(timezone=True), server_default=func.now()) # Timestamp when the story was created
+
+    nodes = relationship("StoryNode", back_populates="story")
+    
